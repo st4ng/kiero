2638311382caced56ffdecc128ee48cc5f3e5b01
st4ng <76733377+st4ng@users.noreply.github.com>
2024-02-20 17:46:54 +0100

    Avoid redefinition of constants

--- kiero.h
+++ kiero.h
@@ -5,13 +5,34 @@
 
 #define KIERO_VERSION "1.2.12"
 
-#define KIERO_INCLUDE_D3D9   0 // 1 if you need D3D9 hook
-#define KIERO_INCLUDE_D3D10  0 // 1 if you need D3D10 hook
-#define KIERO_INCLUDE_D3D11  0 // 1 if you need D3D11 hook
-#define KIERO_INCLUDE_D3D12  0 // 1 if you need D3D12 hook
+#ifndef KIERO_INCLUDE_D3D9
+#define KIERO_INCLUDE_D3D9 0 // 1 if you need D3D9 hook
+#endif
+
+#ifndef KIERO_INCLUDE_D3D10
+#define KIERO_INCLUDE_D3D10 0 // 1 if you need D3D10 hook
+#endif
+
+#ifndef KIERO_INCLUDE_D3D11
+#define KIERO_INCLUDE_D3D11 0 // 1 if you need D3D11 hook
+#endif
+
+#ifndef KIERO_INCLUDE_D3D12
+#define KIERO_INCLUDE_D3D12 0 // 1 if you need D3D12 hook
+#endif
+
+#ifndef KIERO_INCLUDE_OPENGL
 #define KIERO_INCLUDE_OPENGL 0 // 1 if you need OpenGL hook
+#endif
+
+#ifndef KIERO_INCLUDE_VULKAN
 #define KIERO_INCLUDE_VULKAN 0 // 1 if you need Vulkan hook
-#define KIERO_USE_MINHOOK    0 // 1 if you will use kiero::bind function
+#endif
+
+#ifndef KIERO_USE_MINHOOK
+#define KIERO_USE_MINHOOK 0 // 1 if you will use kiero::bind function
+#endif
+
 
 #define KIERO_ARCH_X64 0
 #define KIERO_ARCH_X86 0
